06761352cdb8092621ed265297b98f4bed725082
Jonatas <154271329+Mar1nho1205@users.noreply.github.com>
2025-11-15 16:48:06 -0300

    Update main.js

---
+++ main/main.js
@@ -0,0 +1,33 @@
+const listaRobos = [
+  "img/robotron.png"
+  "img/robotron-amarelo.png"
+  "img/robotron-azul.png"
+  "img/robotron-branco.png"
+  "img/robotron-preto.png"
+  "img/robotron-rosa.png"
+  "img/robotron-vermelho.png"
+];
+
+const roboImg = document.querySelector(".robo")
+const btnAnterior = document.querySelector("#robo-anterior");
+const btnProximo = document.querySelector("#robo-proximo");
+
+let roboAtual = 0;
+
+function trocarRobo(direcao) {
+  if (direcao === "proximo") {
+    roboAtual++;
+
+    if (roboAtual >= listaRobos.length) {
+      roboAtual = 0;
+    }
+  } else if (direcao === "anterior") {
+    roboAtual--;
+
+    if (roboAtual < 0) {
+      roboAtual = listaRobos.length - 1;
+    }
+  }
+
+  roboImg.src = listaRobos[roboAtual];
+}
