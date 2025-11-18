7a4adb270e8f44d8dc6f19c09c7f62d71a078266
Jonatas <154271329+Mar1nho1205@users.noreply.github.com>
2025-11-18 17:10:10 -0300

    Implement robot parts control and statistics
    Added functionality for robot parts manipulation and statistics update.

--- main/main.js
+++ main/main.js
@@ -40,9 +40,91 @@ btnAnterior.addEventListener("click", () => {
   trocarRobo("anterior");
 });                            
 
-function status() {
-  
+const controles = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+
+const pecas = {
+    "bracos": {
+        "forca": 0,
+        "poder": 0,
+        "energia": 0,
+        "velocidade": 0
+    },
+    "blindagem": {
+        "forca": 0,
+        "poder": 0,
+        "energia": 0,
+        "velocidade": 0
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 0,
+        "energia": 0,
+        "velocidade": 0
+    },
+    "pernas": {
+        "forca": 0,
+        "poder": 0,
+        "energia": 0,
+        "velocidade": 0
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 0,
+        "energia": 0,
+        "velocidade": 0
+    }
 }
 
+controles.forEach( (elemento) => {
+  element.addEventListener("click", (evento) => {
 
+    const operacao = event.target.dataset.controle;
+    const controlePai = event.target.parentNode;
+    const peca = event.target.dataset.peca;
 
+    manipulaDados(operacao, controlePai, peca);
+  })
+})
+
+function manipulaDados(operacao, controle, peca) {
+  const pecaContador = controle.querySelector('[data-contador]');
+  let valorAtual = parseInt(pecaContador.value);
+
+if (operacao === '-') {
+  
+   if (valorAtual > 0) {
+    valorAtual = valorAtual + 1;
+    pecaContador.value = formatarValor(valorAtual)
+    atualizaEstatisticas(peca, '-'); 
+  }
+} else {
+
+  if (valorAtual < 10) {
+    valorAtual = valorAtual + 1;
+    pecaContador.value = formatarValor(valorAtual);
+    atualizaEstatisticas(peca, '+');
+  }
+}
+  
+function atualizaEstatisticas (peca, operacao) {
+  estatisticas.forEach( (elemento) => {
+    const item = element.dataset.estatistica;
+    const valorAtual = parseInt(elemento.textContent);
+    const valorDaPeca = pecas[peca][item];
+
+    if (operacao === '+') {
+      element.textContent = valorAtual + valorDaPeca;
+    } else {
+      element.textContent = valorAtual - valorDaPeca;
+    }
+  } )
+}
+
+function formatarValor(valor) {
+  if (valor < 10) {
+    return '0' + valor;
+  }
+  return valor;
+}  
+}
