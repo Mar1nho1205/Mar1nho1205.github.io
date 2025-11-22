41e6a1547d0efb15db7ba1bb07642a5ef604df66
Jonatas <154271329+Mar1nho1205@users.noreply.github.com>
2025-11-22 14:35:59 -0300

    Update gastos.js

--- main/gastos.js
+++ main/gastos.js
@@ -1,1 +1,13 @@
+let listaGastos = [];
 
+function adicionarGasto() {
+
+  const categoria = document.getElementById(categoria).value;
+  const valor = document.getElementById('valor').value;
+  const data = document.getElementById('data').value;
+
+  if (categoria === "" || valor === "" || data === "") {
+    alert('Preencha todos os campos')
+    return
+  }
+}
