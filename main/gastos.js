c0aa0818cb1b266256409cd0eef9696ea0934db1
Jonatas <154271329+Mar1nho1205@users.noreply.github.com>
2025-11-22 15:19:41 -0300

    Update gastos.js

--- main/gastos.js
+++ main/gastos.js
@@ -2,7 +2,7 @@ let listaGastos = [];
 
 function adicionarGasto() {
 
-  const categoria = document.getElementById(categoria).value;
+  const categoria = document.getElementById('categoria').value;
   const valor = document.getElementById('valor').value;
   const data = document.getElementById('data').value;
 
@@ -10,4 +10,94 @@ function adicionarGasto() {
     alert('Preencha todos os campos')
     return;
   }
+
+  const novoGasto = {
+    categoria: categoria,
+    valor: parseFloat(valor),
+    data: data
+  };
+
+  listaGastos.push(novoGasto);
+
+  atualizarTabela();
+  limparCampos();
 }
+
+function limparCampos() {
+  document.getElementById('valor').value = ''
+  document.getElementById('data').value = ''
+  document.getElementById('categoria').focus();
+}
+
+function atualizarTabela() {
+  const tbody = document.getElementById('tabela-corpo');
+  tbody.innerHTML = '';
+
+  listaGastos.forEach(gasto => {
+    const valorFormatado = gasto.valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+    const data = gasto.data;
+    const linha = ` 
+    <tr>
+      <td>${gasto.categoria}</td>
+      <td>${data}</td>
+      <td>${valorFormatado}</td>
+    </tr>
+    `;
+
+    tbody.innerHTML += linha;
+  });
+}
+
+function calulcarResumo() {
+  const areaResumo = document.getElementById('area-resumo');
+  let htmlResumo = '<h3>Resumo</h3>';
+
+  const totalGeral = listaGastos.reduce((acumulador, item) => acumulador + item.valor, 0);
+  htmlResumo += `<p>Total: ${formatarMoeda(totalGeral)}</p>`;
+
+  let totaisPorMes = {};
+
+  listaGastos.forEach(gasto => {
+    const mes = gasto.data.split('-')[1];
+
+    if (!totaisPorMes[mes]) {
+      totaisPorMes[mes] = 0;
+    }
+
+    totaisPorMes[mes] += gasto.valor
+  });
+
+  for (let mes in totaisPorMes) {
+    htmlResumo += `<p>Total no Mês ${mes}: ${formatarMoeda(totaisPorMes[mes])}</p>`;
+  }
+
+  let totaisPorCategoria = {};
+
+  listaGastos.forEach(gasto => {
+    const cat = gasto.categoria;
+    if (!totaisPorCategoria[cat]) {
+      totaisPorCategoria[cat] = 0;
+    }
+  totaisPorCategoria[cat] += gasto.valor;
+  });
+
+  for (let cat in totaisPorCategoria) {
+    htmlResumo += `<p>Total de gastos com ${cat}: ${formatarMoeda(totaisPorCategoria[cat])}</p>`;
+  }
+
+  areaResumo.innerHTML = htmlResumo;
+}
+
+function formatarMoeda(valor) {
+  return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+window.addEventListener('load', () => {
+
+  const btnAdd = document.getElementById('btn-adicionar');
+  const btnCalc = document.getElementById('btn-calcular');
+
+  btnAdd.addEventListener('click', adicionarGasto);
+
+  btnCalc.addEventListener('click', calcularResumo);
+});
